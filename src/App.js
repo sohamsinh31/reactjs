e0c20b5f6d1a2dcf4f23d2442dcfe1484a0473f7
sohamsinh31 <sohamb2019@gmail.com>
2022-03-21 14:06:00 +0530

    2.0

--- src/App.js
+++ src/App.js
@@ -1,55 +1,206 @@
+import React,{ useState} from 'react';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Todos from './components/Todos';
-import React, { useState,useEffect } from 'react';
-import AddTodo from './components/AddTodo';
-
-function App() {
-  let initTodo;
-  if(localStorage.getItem("todos")===null){
-    initTodo = [];
-  }
-  else{
-    initTodo = JSON.parse(localStorage.getItem("todos"));
-  }
-  const[todos, setTodos] = useState(initTodo);
-  const addTodo = (title,desc)=>{
-    let sno;
-    console.log("i am adding todo",title,desc);
-    if(todos.length===0){
-      sno=0;
+import Post from './Post';
+import { useEffect } from 'react';
+import 'firebase/firestore';
+import { getAuth,signOut, onAuthStateChanged , createUserWithEmailAndPassword,updateProfile, signInWithEmailAndPassword } from "firebase/auth";
+import {db ,rdb,storage} from './firebase';
+import { collection, doc, getDocs, setDoc } from "firebase/firestore"; 
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import { Input } from '@mui/material';
+import Imageuplpad from './Imageuplpad';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from 'react-router-dom';
+const App = () => {
+  const [posts,setPosts] = useState([]);
+useEffect(()=>{
+const colref = collection(db,'photos');
+getDocs(colref).then(snapshot=>{
+  setPosts(snapshot.docs.map(doc =>(
+    {
+      id:doc.id,
+      post:doc.data()
     }
-    else{
-     sno = todos[todos.length-1].sno+1; 
+  )))
+})
+},[]);
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+const auth = getAuth();
+const [open, setOpen] = React.useState(false);
+const handleOpen = () => setOpen(true);
+const handleClose = () => setOpen(false);
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+const [email,setEmail] = useState('');
+const [user, setUser] = useState(null);
+const [opensignin, setopensignin] = useState(false);
+const [useremail, setuseremail] = useState(null);
+const [displayusername, setdisplayusername] = useState(null);
+//const user2 = auth.currentUser.displayName;
+useEffect(()=>{
+  // const user2 = auth.currentUser.displayName;
+  onAuthStateChanged(auth, (authUser)=>{
+    
+    if(authUser){
+      //user has logged in
+      setUser(authUser);
+      setdisplayusername(authUser.displayName);
+      setuseremail(authUser.email);
     }
-    const myTodo = {
-      sno: sno,
-      title: title,
-      desc: desc,
+    else {
+      setUser(null);
+      console.log("signed out");
     }
-    setTodos([...todos,myTodo]);
-    console.log(myTodo);
-  }
-  useEffect(() => {
-    localStorage.setItem("todos",JSON.stringify(todos));
-    }, [todos])
-
-  const onDelete = (todo)=>{
-    console.log("I am on delete of todo",todo);
-    setTodos(todos.filter((e)=>{
-      return e!==todo;
-    }));
-    localStorage.setItem("todos",JSON.stringify(todos));
-  }
-  return (
-<>
-<Header/>
-<AddTodo addTodo={addTodo} />
-<Todos todos={todos} onDelete={onDelete} />
-<Footer/>
-</>
-  );
+  })
+},[user,displayusername]);
+const signUp = (event) =>{
+  event.preventDefault();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((authUser) => {
+      // Signed in 
+      console.log("signed in");
+      console.log(authUser);
+       updateProfile(auth.currentUser,{
+          displayName:username
+        })
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(error.message)
+      // ..
+    });
 }
+// const signOut = ()=>{
+//   signOut(auth).then(() => {
+//     // Sign-out successful.
+//   }).catch((error) => {
+//     // An error happened.
+//   });
+// }
+const signIn = (event) =>{
+  event.preventDefault();
+  signInWithEmailAndPassword(auth, email, password)
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(error.message)
+  });
+  setopensignin(false)
+}
+  return (
+    <div className="app">
+    <div className="app_header">
+      <h4>VMEET</h4>
+      {user?(
+            <Button onClick={()=>signOut(auth)}>Signout</Button>
+          
+          ):
+          (
+            <div className="app_login">
+            <Button onClick={()=>setopensignin(true)}>Login</Button>
+            <Button onClick={handleOpen}>SignUp</Button>
+            </div>
+          )
+}
+    </div>
+    {displayusername?(
+    <Imageuplpad email={useremail} username={displayusername} />
+    ):(
+      <h3>sorry you need to login</h3>
+    )
+}
+            <div>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <form className='app_signup'>
+              <h4 style={{
+                textAlign:'center'
+                }}>VMEET</h4>
+<Input
+type="text"
+placeholder='username'
+value={username}
+onChange={(e)=>setUsername(e.target.value)}
+/>
+<Input
+type="email"
+placeholder='email'
+value={email}
+onChange={(e)=>setEmail(e.target.value)}
+/>
+<Input
+type="password"
+placeholder='password'
+value={password}
+onChange={(e)=>setPassword(e.target.value)}
+/>
+<Button type="submit" onClick={signUp}>Signup</Button>
+</form>
+            </Box>
+          </Modal>
+          <Modal
+            open={opensignin}
+            onClose={()=>setopensignin(false)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <form className='app_signup'>
+              <h4 style={{
+                textAlign:'center'
+                }}>VMEET</h4>
+<Input
+type="email"
+placeholder='email'
+value={email}
+onChange={(e)=>setEmail(e.target.value)}
+/>
+<Input
+type="password"
+placeholder='password'
+value={password}
+onChange={(e)=>setPassword(e.target.value)}
+/>
+<Button type="submit" onClick={signUp}>Login</Button>
+</form>
+            </Box>
+          </Modal>
 
+        </div>
+    {
+      posts.map(({post,id}) =>(
+        <Post key={id} postId={id} email={useremail} username={post.username} caption = {post.caption} timestamp={post.timestamp} imageurl={post.imageurl}/>
+      ))
+    }
+{/* <Post  username="son41" caption="hi there" imageurl={vplex} /> */}
+
+    </div>
+)
+}
+function About(){
+  return <h1>hi</h1>
+}
 export default App;
